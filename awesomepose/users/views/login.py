379b9a8cd520d3899e9fe134117f9393f73e33e4
manducku <alwaysfun2183@gmail.com>
2016-04-07 01:02:03 +0900

    add next parameter

--- awesomepose/users/views/login.py
+++ awesomepose/users/views/login.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from users.forms import LoginForm, SignupForm
 from django.views.generic import TemplateView
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 
 class LoginView(TemplateView):
@@ -11,6 +12,7 @@ class LoginView(TemplateView):
     def post(self, request):
         email = request.POST.get("email")
         password = request.POST.get("password")
+        next = request.POST.get("next") or reverse("home")
         user = authenticate(
                 email=email,
                 password=password,
@@ -19,6 +21,6 @@ class LoginView(TemplateView):
         if user:
             messages.success(request, '로그인에 성공하였습니다.')
             login(request, user)
-            return redirect(next_page_url)
+            return redirect(next)
         messages.warning(request, '비밀번호가 틀렸습니다')
         return redirect('login')
