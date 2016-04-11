607479da6442e6377a41ab257349c16d7faa2672
manducku <alwaysfun2183@gmail.com>
2016-04-12 01:58:34 +0900

    add google profile get

--- awesomepose/users/socials/update_avatar.py
+++ awesomepose/users/socials/update_avatar.py
@@ -7,6 +7,10 @@ from social.utils import slugify
 def update_avatar(backend, response, uid, user, *args, **kwargs):
     if backend.name == 'facebook':
         url = 'http://graph.facebook.com/{0}/picture?type=large'.format(response['id'])
+    if backend.name == 'google-oauth2':
+        url = response['image'].get('url')
+        ext = url.split('.')[-1]
+
         profile_image = urlopen(url)
         user.profile_image.save(slugify(user.email + "&social&") + '.jpg', ContentFile(profile_image.read()))
         user.save()
