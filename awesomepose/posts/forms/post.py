30c448a2daf835ec0326377a9fec87dd3d13cbd8
manducku <alwaysfun2183@gmail.com>
2016-04-27 00:47:05 +0900

    add placeholder in url, title

--- awesomepose/posts/forms/post.py
+++ awesomepose/posts/forms/post.py
@@ -18,9 +18,9 @@ class PostForm(forms.ModelForm):
         self.helper.form_method = 'POST'
         self.helper.label_class = 'control-label'
         self.helper.layout = Layout(
-            Field('title', css_class='form-control'),
-            Field('content', css_class='form-control'),
-            Field('product_url', css_class='form-control'),
+            Field('title', css_class='form-control', placeholder="제목을 입력해 주세요"),
+            Field('content', css_class='form-control', ),
+            Field('product_url', css_class='form-control', placeholder="구매처의 주소를 붙여넣어 주세요"),
             FormActions(Submit('save', '저장하기', css_class='btn btn-primary'),
                         Button('cancel', 'Cancel', css_class='btn btn-default')
                         ),
@@ -30,7 +30,8 @@ class PostForm(forms.ModelForm):
         model = Post
         widgets = {
                 'title': forms.TextInput(),
-                'content': SummernoteInplaceWidget(),
+                'content': SummernoteInplaceWidget(
+                    ),
                 'product_url': forms.TextInput(),
                 }
         fields = ['title', 'content', 'product_url']
